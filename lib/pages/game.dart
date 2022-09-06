b278e5451adceb081605462807ed80b9d0292bec
yuika <y.yuika42@outlook.com>
2022-09-06 17:15:29 +0900

    {add 2_1}役職クラスを作成

---
+++ lib/pages/game.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:state_notifier/state_notifier.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class Game extends StateNotifier<int> {
+  Game() : super(0);
+  List<TextEditingController> controllers;
+
+  List<String> _positions = [
+    "村人",
+    "村人",
+    "人狼",
+    "人狼",
+    "占い師",
+    "てるてる",
+  ];
+  List<String> get positions => _positions;
+
+  void shufflePositoins() {
+    _positions.shuffle();
+  }
+
+  void increment() {
+    state++;
+  }
+}
